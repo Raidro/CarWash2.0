d908527f797f8ebfa71df858db7b40394d0739eb
mateus <mateus_sat@hotmail.com>
2019-05-21 21:21:05 -0300

    Criação de 2 rotas exemplo

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,20 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/a', function () {
+    return \App\User::all();
+    return \App\User::find(1);
+    return [
+        'nome' => 'Icaro',
+        'sobrenome' => 'Scherma',
+    ];
+});
+Route::post('/a', function() {
+    $data = request()->validate([
+        'name'      => 'required|string|max:32',
+        'email'     => 'required|string|max:64',
+        'password'  => 'required|string|min:6|max:24',
+    ]);
+    $data['password'] = bcrypt($data['password']);
+    return \App\User::create($data);
+});
