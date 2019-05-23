c8b990e9820671b353602e5e4084373dd1fcbcfe
mateus <mateus_sat@hotmail.com>
2019-05-23 20:30:49 -0300

    criação do PUT e do DELETE

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,32 +2,52 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index(Request $request){
-
-        return \App\User::all();
-
+    public function index(Request $request, User $user)
+    {
+        $users = $user::when(!empty($request->name), function ($query) use ($request) {
+            return $query->where('name', 'like', '%'.$request->name.'%');
+        })->when(!empty($request->email), function ($query) use ($request) {
+            return $query->where('email', 'like', '%'.$request->email.'%');
+        })->latest();
+        return $users->paginate();
     }
 
-    public function store(Request $request){
-
-         
-            $data = $request->validate([
-                 'name'      => 'required|string|max:32',
-                 'email'     => 'required|string|max:64',
-                 'password'  => 'required|string|min:6|max:24',
-             ]);
-                
-             $data['password'] = bcrypt($data['password']);
-             return \App\User::create($data);
-         
-            
-       
+    public function show(User $user)
+    {
+        return $user;
     }
-    public function update(Request $request){}
-        
-    public function destroy(){}
+
+    public function store(Request $request, User $user)
+    {     
+        $data = $request->validate([
+            'name'      => 'required|string|max:32',
+            'email'     => 'required|string|max:64',
+            'password'  => 'required|string|min:6|max:24',
+        ]);
+        $data['password'] = bcrypt($data['password']);
+        return $user::create($data);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $data = $request->validate([
+            'name'      => 'required|string|max:32',
+            'email'     => 'required|string|max:64',
+            'password'  => 'required|string|min:6|max:24',
+        ]);
+        $data['password'] = bcrypt($data['password']);
+        $user->update($data);
+        return response($user, 200);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response(null, 204);
+    }
 }
